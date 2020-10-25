346b363eee593e4ca297b1249a90dbeef581cb5d
jomapormentilla <jomapormentilla@gmail.com>
2020-10-25 18:41:17 -0400

    commit tree d41b0201ac91c749649ae45feeac7ecbbcc833ba

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
 # code here!
+
+class School
+    attr_accessor :name, :roster
+
+    def initialize( name, roster={} )
+        @name = name
+        @roster = roster
+    end
+
+    def add_student( student_name, grade )
+        
+        if @roster[grade] == nil
+            @roster[grade] = []
+        end
+        @roster[grade] << student_name
+        @roster
+    end
+
+    def grade( grade )
+        @roster[grade]
+    end
+
+    def sort
+        hash = @roster
+        hash.each do |grade, students|
+            hash[grade] = students.sort
+        end
+        hash
+    end
+end
